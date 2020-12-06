55af3d12bf33db40ce89e9174aae26f92bc559cf
Sourav Das <iamsouravdaswade420@gmail.com>
2020-12-06 18:51:48 +0530

    faq added email validation done by js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log("Hello");
+const email=document.getElementById("emai");
+const error=document.getElementById("error");
+eve();
+function eve()
+{
+    email.addEventListener("blur",kk);
+}
+function kk()
+{
+	let pattern=/^[^ ]+@[^ ]+\.[a-z]{2,10}$/;
+	let a=email.value;
+	if(a.match(pattern))
+	{
+		// alert("failed");
+		error.innerText="Valid Email";
+	}
+	else
+	{
+		error.innerText="Please enter a valid email";
+		// alert("succse");
+
+	}
+	if(a=="")
+	{
+		error.innerText="Please enter a email";
+
+	}
+}
