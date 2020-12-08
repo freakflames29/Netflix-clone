71b1434d03127bfd3b3418946738c1b6cf9b43e2
Sourav Das <iamsouravdaswade420@gmail.com>
2020-12-08 20:53:15 +0530

    bottom register now section added and js also done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 const email=document.getElementById("emai");
+// const email=document.querySelector(".jamlaga");
 const error=document.getElementById("error");
+
+let email2=document.getElementById("emais");
+const erro2=document.getElementById("error2");
+
 eve();
 function eve()
 {
-    email.addEventListener("blur",kk);
+	email.addEventListener("blur",kk);
+	email2.addEventListener("blur",bb);
+
+	
 }
 function kk()
 {
@@ -22,7 +30,30 @@ function kk()
 	}
 	if(a=="")
 	{
-		error.innerText="Please enter a email";
+		error.innerText="Please enter an email";
+
+	}
+}
+function bb()
+{
+
+	let patterne=/^[^ ]+@[^ ]+\.[a-z]{2,10}$/;
+	let b=email2.value;
+	if(b.match(patterne))
+	{
+		// alert("failed");
+		error2.innerText="Valid Email";
+		// erro2.style.color="green";
+	}
+	else
+	{
+		error2.innerText="Please enter a valid email";
+		// alert("succse");
+
+	}
+	if(b=="")
+	{
+		error2.innerText="Please enter an email";
 
 	}
 }
